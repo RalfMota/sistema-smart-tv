cd9ec98ab5f7790e181b17c2711c019c9074f55f
Ralf Mota <ralfdiegomota@gmail.com>
2024-07-22 22:28:08 -0400

    repositório para exercícios en java

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -6,6 +6,17 @@ public class SmartTv {
 
     int volume = 25;
 
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
     public void aumentarVolume(){
         volume++;
     }
